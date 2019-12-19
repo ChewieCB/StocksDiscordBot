00d0f6ff79f5248827a6a0368d84094e8c858c6d
Jack McCaffrey <45487574+ChewieCB@users.noreply.github.com>
2019-12-19 15:29:08 +0000

    Added APIError catch for expiring sheets auth
    Gspread logs the credentials out after a certain amount of time, if the code is run and throws an APIError now it will log back in and recursively call the write_to_sheet function

--- sheet.py
+++ sheet.py
@@ -35,18 +35,22 @@ def write_to_sheet(email: str, username: str):
     :param username: A valid username string.
     :return: A string for the bot to display on either success or failure.
     """
-    # Open the first sheet of the specified google spreadsheet
-    sheet = SHEET.sheet1
-    # Get the existing emails and usernames to check for the next empty row
-    existing_emails = sheet.col_values(1)
-    existing_usernames = sheet.col_values(2)
-    # Check if email or username already exists in list
-    if email in existing_emails or username in existing_usernames:
-        return "Email and/or username already registered!"
-    else:
-        # Get the next empty row from the last index of the existing rows
-        next_empty_row = len(existing_emails) + 1
-        # Add the new email and username to the next empty row (arg order is row number, col number, value)
-        sheet.update_cell(next_empty_row, 1, email)
-        sheet.update_cell(next_empty_row, 2, username)
-        return "Email and username registered, "
+    try:
+        # Open the first sheet of the specified google spreadsheet
+        sheet = SHEET.sheet1
+        # Get the existing emails and usernames to check for the next empty row
+        existing_emails = sheet.col_values(1)
+        existing_usernames = sheet.col_values(2)
+        # Check if email or username already exists in list
+        if email in existing_emails or username in existing_usernames:
+            return "Email and/or username already registered!"
+        else:
+            # Get the next empty row from the last index of the existing rows
+            next_empty_row = len(existing_emails) + 1
+            # Add the new email and username to the next empty row (arg order is row number, col number, value)
+            sheet.update_cell(next_empty_row, 1, email)
+            sheet.update_cell(next_empty_row, 2, username)
+            return "Email and username registered, "
+    except APIError:
+        gc.login()
+        write_to_sheet(email, username)
